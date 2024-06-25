78ca7ebddf2b442ec2a8ba92b68086c905512cca
Kyle Loggenberg <kyleloggenberg9@gmail.com>
2024-06-25 13:38:04 +0200

    Just startng up
    This project will be a GUI mastermind

---
+++ main.py
@@ -0,0 +1,31 @@
+# import random
+
+def open_file(file_name):
+    pass
+
+
+def get_word():
+    pass
+
+
+def get_user_input():
+    # while(True):
+    #     user_input = input("Please ennter a letter:\n").lower()
+    #     if user_input.isdigit:
+    pass
+
+
+def check_guess(guessed_letter, word):
+    for letter in word:
+        if guessed_letter == letter:
+            return True
+    return False
+
+
+def main():
+    # word  = "cat"
+    get_user_input()
+
+
+if __name__ == "__main__":
+    main()
